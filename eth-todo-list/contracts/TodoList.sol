94b059b85b75d9946323b93118ab30f07fb280ee
Alex Gear <alexgear@gmail.com>
2021-07-18 18:28:17 -0400

    Add /src

--- eth-todo-list/contracts/TodoList.sol
+++ eth-todo-list/contracts/TodoList.sol
@@ -1,5 +1,26 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
+	// State variable
 	uint public taskCount = 0;
+
+	struct Task {
+		uint id;
+		string content;
+		bool completed;
+	}
+
+	// State variable
+	mapping(uint => Task) public tasks;
+
+	function createTask(string memory _content) public {
+		taskCount ++;
+		tasks[taskCount] = Task(taskCount, _content, false);
+	}
+
+	// Constructor function
+	constructor() public {
+		createTask("Check out dappuniversity.com");
+	}
+
 }
